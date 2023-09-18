4a4fb9c2a6c1a6dad2304e82de54a9086a1d473d
phaelz <raphaelmacielmenger@gmail.com>
2023-09-18 19:58:18 -0300

    Now he asks if you want to continue

---
+++ src/Message.java
@@ -0,0 +1,47 @@
+public class Message {
+    public void sendMessages (){
+        System.out.println("Selecione a opção desejada:");
+
+        System.out.println("1. Celsius para Fahrenheit");
+        System.out.println("2. Fahrenheit para Celsius");
+        System.out.println("3. Celsius para Kelvin");
+        System.out.println("4. Kelvin para Celsius");
+
+        System.out.println("5. Quilograma para Grama");
+        System.out.println("6. Grama para Quilograma");
+        System.out.println("7. Quilograma para Tonelada");
+        System.out.println("8. Tonelada para Quilograma");
+
+        System.out.println("9. Metro para Quilômetro");
+        System.out.println("10. Metro para Decímetro");
+        System.out.println("11. Metro para Centímetro");
+        System.out.println("12. Metro para Milímetro");
+
+        System.out.println("13. Quilômetro para Metro");
+        System.out.println("14. Quilômetro para Centímetro");
+        System.out.println("15. Quilômetro para Decímetro");
+        System.out.println("16. Quilômetro para Milímetro");
+
+        System.out.println("17. Centímetro para Quilômetro");
+        System.out.println("18. Centímetro para Metro");
+        System.out.println("19. Centímetro para Decímetro");
+        System.out.println("20. Centímetro para Milímetro");
+
+        System.out.println("21. Decímetro para Metro");
+        System.out.println("22. Decímetro para Centímetro");
+        System.out.println("23. Decímetro para Milímetro");
+        System.out.println("24. Decímetro para Quilômetro");
+
+        System.out.println("25. Milímetro para Decímetro");
+        System.out.println("26. Milímetro para Metro");
+        System.out.println("27. Milímetro para Quilômetro");
+        System.out.println("28. Milímetro para Centímetro");
+
+        System.out.println("29. Ano-luz para Metro");
+        System.out.println("30. Ano-luz para Quilômetro");
+        System.out.println("31. Metro para Ano-luz");
+        System.out.println("32. Quilômetro para Ano-luz");
+
+        System.out.println("33. Finalizar");
+    }
+}
